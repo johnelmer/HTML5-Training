a54e83815b9ed4c7bebd669bc3beebe17d664e5f
johnelmer <johnelmer_loretizo@yahoo.com>
2017-06-10 15:17:01 +0800

    feat(Squares): Every 7th square has 4 little squares

--- Day3/Squares/functionality.js
+++ Day3/Squares/functionality.js
@@ -15,11 +15,29 @@ function drawSquares() {
   var c = document.getElementById("mycanvas");
   var ctx = c.getContext("2d");
   ctx.beginPath();
+  var count = 0;
   var k = 0;
   while (k < c.height) {
-    for (var i = 0; i < c.width; i++) {
+    for (var i = 0; i < c.width / 100; i++) {
       ctx.fillStyle= '#' + Math.floor((Math.random()*16777215)).toString(16);
       ctx.fillRect(i * 100, k, 100,100);
+      if (count % 7 == 0) {
+        if (count == 0) {
+          count++;
+        }
+        for (var j = 0; j < 2; j++) {
+          for (var l = 0; l < 2; l++) {
+            var additionX = i * 100;
+            additionX = (j * 50) + additionX;
+            var additionY = k;
+            additionY = additionY + (l * 50);
+            ctx.fillStyle= '#' + Math.floor((Math.random()*16777215)).toString(16);
+            ctx.fillRect(additionX, additionY, 50,50);
+          }
+        }
+      }
+      count++;
+      console.log(count);
     }
     k += 100;
   }
