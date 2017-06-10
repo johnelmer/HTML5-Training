92b591a5c90619b41b5587b36eb14507c4164d60
johnelmer <johnelmer_loretizo@yahoo.com>
2017-06-10 17:03:04 +0800

    fix(MovingBall): Adds a rotating square

--- Day3/MovingBall/functionality.js
+++ Day3/MovingBall/functionality.js
@@ -1,6 +1,6 @@
-var xPos = 32;
-var yPos = 32;
-var dim = 100;
+var xPos = 0;
+var yPos = 0;
+var dim = 64;
 var c;
 window.onload = function() {
   c = document.getElementById("mycanvas");
@@ -8,7 +8,6 @@ window.onload = function() {
   ctx.fillStyle = "#00FF00";
   ctx.fillRect(xPos, yPos, dim, dim);
   ctx.fill();
-
 }
 // window.addEventListener('mousedown', function(event) {
 //   // var c = document.getElementById("mycanvas");
