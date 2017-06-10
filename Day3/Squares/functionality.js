3cec4877d54c8ecc6b8a4a240a2a2da2b6bad19a
johnelmer <johnelmer_loretizo@yahoo.com>
2017-06-10 14:50:20 +0800

    feat(MultipleSquares): Multiple randomly generated squares

--- Day3/Squares/functionality.js
+++ Day3/Squares/functionality.js
@@ -9,18 +9,18 @@ function on_resize() {
   var c = document.getElementById("mycanvas");
   c.width = window.innerWidth;
   c.height = window.innerHeight;
-  drawSquares
+  drawSquares();
 }
 function drawSquares() {
   var c = document.getElementById("mycanvas");
   var ctx = c.getContext("2d");
   ctx.beginPath();
-  ctx.fillStyle="#FF0000";
-  ctx.fillRect(0, 0, 100,100);
-  ctx.fillRect( 100,100, 100,100);
-
-  ctx.fillRect(200, 200, 100,100);
-
-  ctx.fillRect(300, 300, 100,100);
-
+  var k = 0;
+  while (k < c.height) {
+    for (var i = 0; i < c.width; i++) {
+      ctx.fillStyle= '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      ctx.fillRect(i * 100, k, 100,100);
+    }
+    k += 100;
+  }
 }
