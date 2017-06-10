42d0d9c76d7ae0fc3df5cc12fbbe7e0fc985c129
johnelmer <johnelmer_loretizo@yahoo.com>
2017-06-10 12:06:09 +0800

    feat(MovingRectangle): Responsive based on window dimensions

--- Day3/MovingRectangle/functionality.js
+++ Day3/MovingRectangle/functionality.js
@@ -3,21 +3,27 @@ var x = 10;
 var y = 10;
 var xMov = 1;
 var yMov = 1;
+function on_resize() {
+  var c = document.getElementById("mycanvas");
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  move();
+}
 function move() {
   var c = document.getElementById("mycanvas");
-  var ctx = c.getContext("2d");
-  c.width = window.innerWidth;;
+  c.width = window.innerWidth;
   c.height = window.innerHeight;
+  var ctx = c.getContext("2d");
   ctx.fillStyle = color;
   x += xMov;
-  if (x + 100 > c.width) {
+  if (x + 100 > window.innerWidth) {
     xMov = -1;
   }
   if (x < 0) {
     xMov = 1;
   }
   y += yMov;
-  if (y + 100 > c.height) {
+  if (y + 100 > window.innerHeight) {
     yMov = -1;
   }
   if (y < 0) {
@@ -30,5 +36,9 @@ function move() {
 }
 window.onload = function() {
   setInterval("move()", 5);
+  on_resize();
   move();
+}
+window.onresize = function() {
+  on_resize();
 }
